60405173b7029c59cf3181daa2b135418fd94527
eposha <freedoctime@gmail.com>
2020-01-15 14:05:11 +0300

    5

---
+++ lesson5_events/task1/src/GoodButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const GoodButton = () => {
+  return (
+    <button className="fancy-button" onClick={() => alert("Good Job!")}>
+      Click me!
+    </button>
+  );
+};
+export default GoodButton;
