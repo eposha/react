ce807334455b59bdcdd8b2e27bc8772b1882c9bd
eposha <freedoctime@gmail.com>
2020-01-17 18:11:22 +0300

    5

---
+++ lesson7_render_list/hw1/src/TransactionsList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import moment from "moment";
+
+const TransactionsList = ({ transactions }) => {
+  return transactions.map(obj => (
+    <li key={obj.id} className="transaction">
+      <span className="transaction__date">
+        {moment(obj.time).format("DD MMM")}
+      </span>
+      <span className="transaction__time">
+        {moment(obj.time).format("HH mm")}
+      </span>
+      <span className="transaction__assets">{`${obj.from} → ${obj.to}`}</span>
+      <span className="transaction__rate">{obj.rate}</span>
+      <span className="transaction__amount">{obj.amount}</span>
+    </li>
+  ));
+};
+
+export default TransactionsList;
---
+++ lesson7_render_list/hw2/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import Transaction from "./Transaction";
+
+const rootElement = document.querySelector("#root");
+
+ReactDOM.render(<Transaction />, rootElement);
