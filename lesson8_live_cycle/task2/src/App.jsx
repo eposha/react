a6bee2a329d9f75993fe6cb53d23228a423c8f4c
eposha <freedoctime@gmail.com>
2020-01-21 21:49:14 +0300

    5

--- lesson8_live_cycle/task2/src/App.jsx
+++ lesson8_live_cycle/task2/src/App.jsx
@@ -2,23 +2,10 @@ import React, { Component } from "react";
 import Clock from "./Clock";
 
 class App extends Component {
-  state = {
-    visible: true
-  };
-
-  toggle = () => {
-    this.setState({
-      visible: !this.state.visible
-    });
-  };
-
   render() {
     return (
       <>
-        <button className="btn" onClick={this.toggle}>
-          Toggle
-        </button>
-        <div>{this.state.visible && <Clock />}</div>
+        <Clock />
       </>
     );
   }
