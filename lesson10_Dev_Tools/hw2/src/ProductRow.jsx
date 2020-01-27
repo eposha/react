0c68a61356669f57e5db3009ad5ab71d4468e76b
eposha <freedoctime@gmail.com>
2020-01-27 16:25:42 +0300

    5

--- lesson10_Dev_Tools/hw2/src/ProductRow.jsx
+++ lesson10_Dev_Tools/hw2/src/ProductRow.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 
 const ProductRow = ({ product }) => {
-  const name = product.stocked ? (
-    product.name
-  ) : (
-    <span style={{ color: "red" }}>{product.name}</span>
-  );
+  const { stocked, name, price } = product;
+  const names = stocked ? name : <span style={{ color: "red" }}>{name}</span>;
 
   return (
     <tr>
-      <td>{name}</td>
-      <td>{product.price}</td>
+      <td>{names}</td>
+      <td>{price}</td>
     </tr>
   );
 };
